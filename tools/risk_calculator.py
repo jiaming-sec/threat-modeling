4b1a5428668cc6748d26d690dc7ec0d4f46fabe9
Jiaming Sec <jiamingqu0728@gmail.com>
2025-03-21 23:27:36 -0400

    Update risk_calculator.py

--- tools/risk_calculator.py
+++ tools/risk_calculator.py
@@ -2,3 +2,9 @@
 
 def calculate_risk(likelihood, impact):
     return likelihood * impact
+    
+threats = [
+    {"name": "SQL Injection", "likelihood": 8, "impact": 9},
+    {"name": "DDoS Attack", "likelihood": 7, "impact": 10},
+    {"name": "Insider Threat", "likelihood": 4, "impact": 7},
+]
